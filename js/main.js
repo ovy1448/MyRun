b8681a9bb3a075c7691ee0c9eca38ecf2df59f22
Daniel Ovári <dovari@seznam.cz>
2018-11-16 16:18:27 +0100

    Working form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-$("#add").click(function(){
-    console.log("H")
-})
+$("#add").click(function () {
+    $("#container").html('<form id="addForm">' +
+        'Distance: <input id="distance" type="text"><br>' +
+        'Time: <input id="time" type="text"><br>' +
+        '<input type="submit" value="Submit">' +
+        '</form>'
+    );
+    $("#addForm").submit(function (e) {
+        e.preventDefault();
+        var distance = $("#distance").val();
+        var time = $("#time").val();
+
+        var data = {
+            distance: distance,
+            time: time
+        };
+        console.log(data);
+    });
+});
+$("#home").click(function () {
+    $("#addForm").hide();
+
+});
+
